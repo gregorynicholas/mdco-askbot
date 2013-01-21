aef30511a37cc08a60a55abebe7f2bae0416a959
Gregory Nicholas <faction.gregory@gmail.com>
2013-01-21 02:22:55 -0800

    moving style, images under media/

--- settings.py
+++ settings.py
@@ -105,7 +105,7 @@ TEMPLATE_LOADERS = (
   'django.template.loaders.app_directories.load_template_source',
   # below is askbot stuff for this tuple..
   'askbot.skins.loaders.load_template_source',
-  # 'django.template.loaders.eggs.load_template_source',
+  'django.template.loaders.eggs.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
